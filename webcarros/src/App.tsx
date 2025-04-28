fcd01447bd266a3a69ba057538c4db7cfa77150c
Arthur De Lavor <arthurdelavor100@gmail.com>
2025-04-27 23:50:36 -0300

    feat: adds new components and integrates Swiper

--- webcarros/src/App.tsx
+++ webcarros/src/App.tsx
@@ -1,9 +1,46 @@
-function App() {
-  return (
-    <div>
-      <h1>WebCarros</h1>
-    </div>
-  )
-}
+import {createBrowserRouter} from 'react-router-dom'
+import {CarDetails} from './pages/car'
+import {Dashboard} from './pages/dashboard'
+import {New} from './pages/dashboard/new'
+import {Home} from './pages/home'
+import {Login} from './pages/login'
+import {Register} from './pages/register'
 
-export default App
+import {Layout} from './components/layout'
+import { Private } from './routes/Private'
+
+const router = createBrowserRouter ([
+  {
+    element: <Layout/>,
+    children: [
+        {
+          path: '/', 
+          element: <Home/>
+        },
+        {
+          path: '/car/:id',
+          element: <CarDetails/>
+        },
+        {
+          path: '/dashboard',
+          element: <Private><Dashboard/></Private> 
+        },
+        {
+          path: '/dashboard/new',
+          element: <Private> <New/> </Private>
+        },
+          ]
+
+       },
+
+        {
+        path: '/login',
+        element: <Login/>
+        },
+        {
+        path: '/register',
+        element: <Register/>
+  }
+])
+
+export {router}
