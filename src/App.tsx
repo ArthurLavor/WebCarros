1b7923e78370b0537d9f292f7a3dc4f1ee22c828
Arthur De Lavor <arthurdelavor100@gmail.com>
2025-04-29 12:25:58 -0300

    fix: resolving bugs in firestore connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,45 @@
-import {createBrowserRouter} from 'react-router-dom'
-import {CarDetails} from './pages/car'
-import {Dashboard} from './pages/dashboard'
-import {New} from './pages/dashboard/new'
-import {Home} from './pages/home'
-import {Login} from './pages/login'
-import {Register} from './pages/register'
 
-import {Layout} from './components/layout'
+import { createBrowserRouter } from 'react-router-dom'
+import { Home } from './pages/home'
+import { Login } from './pages/login'
+import { Register } from './pages/register'
+import { Dashboard } from './pages/dashboard'
+import { New } from './pages/dashboard/new'
+import { CarDetail } from './pages/car'
+
+import { Layout } from './components/layout'
 import { Private } from './routes/Private'
 
-const router = createBrowserRouter ([
+const router = createBrowserRouter([
   {
     element: <Layout/>,
     children: [
-        {
-          path: '/', 
-          element: <Home/>
-        },
-        {
-          path: '/car/:id',
-          element: <CarDetails/>
-        },
-        {
-          path: '/dashboard',
-          element: <Private><Dashboard/></Private> 
-        },
-        {
-          path: '/dashboard/new',
-          element: <Private> <New/> </Private>
-        },
-          ]
-
-       },
-
-        {
-        path: '/login',
-        element: <Login/>
-        },
-        {
-        path: '/register',
-        element: <Register/>
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/car/:id",
+        element: <CarDetail/>
+      },
+      {
+        path: "/dashboard",
+        element: <Private><Dashboard/></Private>
+      },
+      {
+        path: "/dashboard/new",
+        element: <Private><New/></Private>
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/register",
+    element: <Register/>
   }
 ])
 
-export {router}
+export { router };
--- src/pages/car/index.tsx
+++ src/pages/car/index.tsx
@@ -1,176 +1,167 @@
-import {useEffect, useState} from "react"
-import {Container} from "../../components/container"
-import { useParams } from "react-router-dom"
-import {FaWhatsapp} from "react-icons/fa"
-import {getDoc, doc, } from "firebase/firestore"
-import { db } from "../../services/firebaseConnection"
-import { useNavigate } from "react-router-dom" 
-import { Swiper, SwiperSlide } from "swiper/react"
+import { useEffect, useState } from 'react'
+import { Container } from '../../components/container'
+import { FaWhatsapp } from 'react-icons/fa'
+import { useNavigate, useParams } from 'react-router-dom'
+
+import { getDoc, doc, } from 'firebase/firestore'
+import { db } from '../../services/firebaseConnection'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 interface CarProps{
-id: string;
-name: string;
-model: string;
-year: string;
-city: string;
-km: string;
-description: string;
-price: string|number;
-created: string;
-owner: string;
-uid: string;
-images: ImagesCarProps[];
-whatsapp: string;
-
+  id: string;
+  name: string;
+  model: string;
+  city: string;
+  year: string;
+  km: string;
+  description: string;
+  created: string;
+  price: string | number;
+  owner: string;
+  uid: string;
+  whatsapp: string;
+  images: ImagesCarProps[]
 }
 
 interface ImagesCarProps{
-uid: string;
-name: string;
-url: string;
+  uid: string;
+  name: string;
+  url: string;
 }
 
+export function CarDetail() {
+  const { id } = useParams(); 
+  const [car, setCar] = useState<CarProps>()
+  const [sliderPerView, setSliderPerView] = useState<number>(2);
+  const navigate = useNavigate();
 
-export function CarDetails() {
-const {id} = useParams()
-const [car, setCar] = useState<CarProps>()
-const [sliderPerview, setSliderPerview] = useState<number>(2)
-const navigate = useNavigate();
 
-useEffect(()=>{
+  useEffect(() => {
     async function loadCar(){
-        if(!id){return}
+      if(!id){ return }
 
-        const docRef = doc(db, "cars", id)
-        getDoc(docRef)
-        .then((snapshot)=>{
+      const docRef = doc(db, "cars", id)
+      getDoc(docRef)
+      .then((snapshot) => {
 
-            if(!snapshot.data()){
-                navigate("/")
-            }
-    
+        if(!snapshot.data()){
+          navigate("/")
+        }
 
-
-            setCar({
-                id: snapshot.id,
-                name: snapshot.data()?.name,
-                year: snapshot.data()?.year,
-                city: snapshot.data()?.city,
-                model: snapshot.data()?.model,
-                uid: snapshot.data()?.uid,
-                description: snapshot.data()?.description,
-                created: snapshot.data()?.created,
-                whatsapp: snapshot.data()?.whatsapp,
-                price: snapshot.data()?.price,
-                km: snapshot.data()?.km,
-                owner: snapshot.data()?.owner,
-                images: snapshot.data()?.images
-            })
+        setCar({
+          id: snapshot.id,
+          name: snapshot.data()?.name,
+          year: snapshot.data()?.year,
+          city: snapshot.data()?.city,
+          model: snapshot.data()?.model,
+          uid: snapshot.data()?.uid,
+          description: snapshot.data()?.description,
+          created: snapshot.data()?.created,
+          whatsapp: snapshot.data()?.whatsapp,
+          price: snapshot.data()?.price,
+          km: snapshot.data()?.km,
+          owner: snapshot.data()?.owner,
+          images: snapshot.data()?.images
         })
+      })
+
+
     }
 
     loadCar()
-},[id])
 
-   useEffect(()=>{
-      function handleResize(){
-          if(window.innerWidth < 720){
-              setSliderPerview(1)
-          }else{
-              setSliderPerview(2)
-          }
+
+  }, [id])
+
+
+  useEffect(() => {
+
+    function handleResize(){
+      if(window.innerWidth < 720){
+        setSliderPerView(1);
+      }else{
+        setSliderPerView(2);
       }
+    }
 
-      handleResize();
+    handleResize();
 
-      window.addEventListener("resize", handleResize)
+    window.addEventListener("resize", handleResize)
 
-      return ()=> {
-        window.removeEventListener("resize", handleResize)
-      }
-   },[])
+    return() => {
+      window.removeEventListener("resize", handleResize)
+    }
 
-return(
+  }, [])
+
+
+  return (
     <Container>
-
-    {car && (
+      
+      { car && (
         <Swiper
-        slidesPerView={sliderPerview}
-        pagination={{ clickable: true }}
-        navigation
+          slidesPerView={sliderPerView}
+          pagination={{ clickable: true }}
+          navigation
         >
-        {car?.images?.map(image=>(
+          {car?.images.map( image => (
             <SwiperSlide key={image.name}>
-            <img 
-            className="w-full h-96 object-cover" 
-            src={image.url} 
-            alt="imagem do carro" />
+              <img
+                src={image.url}
+                className="w-full h-96 object-cover"
+              />
             </SwiperSlide>
-        ))}
-
+          ))}
         </Swiper>
-     )}
+      )}
 
+      { car && (
+      <main className="w-full bg-white rounded-lg p-6 my-4">
+        <div className="flex flex-col sm:flex-row mb-4 items-center justify-between">
+          <h1 className="font-bold text-3xl text-black">{car?.name}</h1>
+          <h1 className="font-bold text-3xl text-black">R$ {car?.price}</h1>
+        </div>
+        <p>{car?.model}</p>
+        
+        <div className="flex w-full gap-6 my-4">
+          <div className="flex flex-col gap-4">
+            <div>
+              <p>Cidade</p>
+              <strong>{car?.city}</strong>
+            </div> 
+            <div>
+              <p>Ano</p>
+              <strong>{car?.year}</strong>
+            </div> 
+          </div>
 
-        {car && (
-            <main className="w-full bg-white rounded-lg p-6 my-4">
-            <div className="flex flex-col sm:flex-row mb-4 items-center justify-between">
-                <h1 className="font-bold text-3xl text-black">{car?.name}</h1>
-                <h1 className="font-bold text-3xl text-black">{car?.price}</h1>
-            </div>
-            <p>{car?.model}</p>
+          <div className="flex flex-col gap-4">
+            <div>
+              <p>KM</p>
+              <strong>{car?.km}</strong>
+            </div> 
+          </div>
+        </div>
 
-            <div className=" flex w-full gap-6 my-4">
-                <div className="flex flex-col gap-4">
-                <div>
-                    <p>Cidade</p>
-                    <strong>{car?.city}</strong>
-                </div>
-                <div>
-                    <p>Ano</p>
-                    <strong>{car?.year}</strong>
-                </div>    
-                </div> 
+        <strong>Descrição:</strong>
+        <p className="mb-4">{car?.description}</p>
+        
 
-                <div className="flex flex-col gap-4">
-                <div>
-                    <p>KM</p>
-                    <strong>{car?.km}</strong>
-                </div>   
-                </div>
-            </div>
+        <strong>Telefone / WhatsApp</strong>
+        <p>{car?.whatsapp}</p>
 
-            <strong>Descrição:</strong>
-            <p className="mb-4">{car?.description}</p>
+        <a
+          href={`https://api.whatsapp.com/send?phone=${car?.whatsapp}&text=Olá vi esse ${car?.name} no site WebCarros e fique interessado!`}
+          target="_blank"  
+          className="cursor-pointer bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-lg font-medium"
+        >
+          Conversar com vendedor
+          <FaWhatsapp size={26} color="#FFF" />
+        </a>
 
-            <strong>Telefone / Whatsapp:</strong>
-            <p>{car?.whatsapp}</p>
-
-            <a 
-            href={`https://api.whatsapp.com/send?phone=${car?.whatsapp}&text=Olá vi esse ${car?.name} e  fiquei interessado!`}
-            target="_blank"
-            className=" curor-pointer bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-lg font-medium">
-                Conversar com o vendedor
-                <FaWhatsapp size={26} color="#fff" />
-            </a>
-            </main>  
-        )}
+      </main>
+      )}
     </Container>
-)
+  )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
